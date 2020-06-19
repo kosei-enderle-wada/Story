7af0b48c37d9887809a68578a51ec5be4329a622
和田エンデルレ晃世 <kosei@wadaenderureakiyonoMacBook-Pro.local>
2020-06-19 13:11:07 +0900

    initial commit

---
+++ Testgit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testgit
+//
+//  Created by 和田エンデルレ晃世 on 2020/06/19.
+//  Copyright © 2020 和田エンデルレ晃世. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
